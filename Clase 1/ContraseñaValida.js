acaa8f1f6950bc55b8ac940f9bd86cbc53e9c4e7
julianmillan44 <149098880+julianmillan44@users.noreply.github.com>
2024-03-12 13:39:12 -0500

    Add files via upload

---
+++ Clase 1/ContraseñaValida.js
@@ -0,0 +1,17 @@
+function contrasenaValida(){
+
+    var contrasenaValida;
+    
+    if (contrasenaValida === "2Fj(jjbFsuj" || contrasenaValida === "eoZiugBf&g9"){
+    return true;
+    }else{
+        return false;
+    }
+    
+}
+
+console.log(contrasenaValida("2Fj(jjbFsuj"))
+console.log(contrasenaValida("eoZiugBf&g9")) 
+console.log(contrasenaValida("hola")) 
+console.log(contrasenaValida("")) 
+
